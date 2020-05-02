2031c8933010d88bd89cc62c62ec2076d7bf8613
Andrew Chalfant <andrewlchalfant@gmail.com>
2020-05-01 21:39:38 -0400

    pull from sheets

--- p5_test.js
+++ p5_test.js
@@ -9,16 +9,35 @@ let r = 100;
 let angle = 30;
 let tex;
 
+let url = "https://sheets.googleapis.com/v4/spreadsheets/1tjvDbvUsSogN2CK5RrXXntTHlWSRl-1T4UiUJJLw5gA/values/Sheet1!";
+let range = "B2:B10";
+let key = "?key=###";
+let sheets = url + range + key;
+let flip = true; 
+let z;
+
 function setup() 
 {
   createCanvas(w, h, WEBGL);
   tex = createGraphics(800, 800);
+  httpGet(sheets, 'jsonp', false, function(response) {
+    resp = response;
+  });
 }
 
 function draw() 
 {
+  if (!resp) {
+    return;
+  }
+  if (flip) {
+    z = convert(resp.values);
+    flip = false;
+  }
+  
   background(0);
   push();
+  
   //translate(200*100, 200)
   let x = r + c * cos(a);
 
@@ -40,4 +59,15 @@ function draw()
 
   angle += 0.0010*10;
   pop();
+}
+
+function convert(z){
+  let arr = [];
+  for (let i = 0; i < z.length; i++) {
+    var str = z[i][0];
+    var tweet = str.split("]")[1];
+    arr.push(tweet);
+  }
+  print(arr);
+  return arr;
 }
