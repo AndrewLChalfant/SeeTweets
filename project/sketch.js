adaf3965aea6a002aa69b8159ecbd0815feaae72
Andrew Chalfant <andrewlchalfant@gmail.com>
2020-05-19 20:38:15 -0400

    seeding

--- project/sketch.js
+++ project/sketch.js
@@ -16,43 +16,35 @@
  * 
  * 
  */
- 
+p5.disableFriendlyErrors = true;
 let resp;
 var x = 0;
 let flip = true; 
 let dataMap;
+
 let url = "https://sheets.googleapis.com/v4/spreadsheets/1tjvDbvUsSogN2CK5RrXXntTHlWSRl-1T4UiUJJLw5gA/values/Sheet4!";
 var r_max = 100; //how many datapoints to fetch
 let range = "A2:C13";// + r_max;
 let key = "?key=AIzaSyCEQ1fTLIunpWw7aMdFXgfyQ6lvkN4kiZc";
 let sheets = url + range + key;
 var easycam;
+
 let params = {
-    radius: 200,
-    radiusMin: 50,
-    radiusMax: 2000
+    scale: 1,
+    scaleMin: 0,
+    scaleMax: 10,
+    seed: 0,
+    seedMin: 0,
+    seedMax: 100,
 };
 
-function preload() {
-  twitter = loadImage("../twitter__.png");}
-    var arr = [0.319,
-    0.314,
-    0.31,
-    0.34,
-    0.315,
-    0.312,
-    0.306,
-    0.304,
-    0.31,
-    0.282,
-    0.297,
-    0.306,];
 function setup() { 
+
+
   pixelDensity(1);
+
   var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   setAttributes('antialias', true);
-    x = random(width);
-  y = random(height);
   
   // define initial state
   var state = {
@@ -63,72 +55,82 @@ function setup() {
   httpGet(sheets, 'jsonp', false, function(response) {
     resp = response;
   });
-  //easycam = new Dw.EasyCam(this._renderer, state);
+
+  easycam = new Dw.EasyCam(this._renderer, state);
   
   // slower transitions look nicer in the ortho mode
-//  easycam.setDefaultInterpolationTime(2000); //slower transition
+  easycam.setDefaultInterpolationTime(2000); //slower transition
   // start with an animated rotation
- // easycam.setRotation(Dw.Rotation.create({angles_xyz:[PI/2, PI/2, PI/2]}), 2500);
- // easycam.setDistance(400, 2500);
- // gui = createGui();
-  //gui.addObject(params);
+  easycam.setRotation(Dw.Rotation.create({angles_xyz:[PI/2, PI/2, PI/2]}), 2500);
+  easycam.setDistance(400, 2500);
+
+  gui = createGui();
+  gui.addObject(params);
 } 
+
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   easycam.setViewport([0,0,windowWidth, windowHeight]);
 }
+
+
 function draw(){
   
-  //if (!resp) {
-  // return; //wait for http response
-  //}
-  //if (flip) {
-  // dataMap = convert(resp.values); 
-  //  flip = false;
-  //}
-//  var cam_dist = easycam.getDistance();
-  //var oscale = cam_dist * 0.001;
-//  var ox = width  / 2 * oscale;
-  //var oy = height / 2 * oscale;
- // ortho(-ox, +ox, -oy, +oy, -10000, 10000);
-  //easycam.setPanScale(0.004 / sqrt(cam_dist));
+  if (!resp) {
+    return; //wait for http response
+  }
+  if (flip) {
+    dataMap = convert(resp.values); 
+    flip = false;
+  }
+
+  var cam_dist = easycam.getDistance();
+  var oscale = cam_dist * 0.001;
+  var ox = width  / 2 * oscale;
+  var oy = height / 2 * oscale;
+  ortho(-ox, +ox, -oy, +oy, -10000, 10000);
+  easycam.setPanScale(0.004 / sqrt(cam_dist));
   
-  background(255);
+	background(255);
   noStroke();
-  
+
   ambientLight(200);
- pointLight(255, 255, 255, 0, 0, 0);
-  
+  pointLight(255, 255, 255, 0, 0, 0);
+
   // objects
   noStroke();
   randomSeed(2);
   noStroke();
-  randomSeed(63);
-  background(255);
-  for(var i = 0; i < 50; i++){
-    var m = 250;
-    var tx = random(-m, m);
-    var ty = random(-m, m);
-    var tz = random(-m, m);
-    var r = ((tx / m) * 0.5 + 0.5) * 255;
-    var g = ((ty / m) * 0.5 + 0.5) * r/2;
-    var b = ((tz / m) * 0.5 + 0.5) * g*6;
-    push();
-    translate(tx, ty, tz-250);
-    ambientMaterial(r,g,b);
+  randomSeed(params.seed);
+  background(0);
+
+  for(var i = 0; i < 60; i++){
+
     //iterate over every day and scale sphere based on positivity data
-    for (var x in arr) {
-      var positive = arr[x];
-      sphere(random(positive - 0.3) * params.radius);
+    for (var key in dataMap) {
+      var m = 100;
+      var tx = random(-m, m);
+      var ty = random(-m, m);
+      var tz = random(-m, m);
+
+      translate(tx, ty, tz);
+      var r = 50;
+      var g = 20;
+      var b = 20;
+      var positive = Number(dataMap[key]);
+      var scale = Math.pow(1.0 / positive, 7) / 20;
+      if (positive > 0.29) {
+        r = random(0, 5) + scale;
+      } else {
+        g = random(0, 5) + scale;
+      }
+      ambientMaterial(r,g,b);
+      sphere(random(0, 5) + Math.pow(scale/100,2) * params.scale);
     }
-    pop();
   }
-  camera()
-  smooth(6400);
-  ambientLight(10000);
- //rotate(300);
- image(twitter, x-450, y-1050);
 }
+
 //get tweets from google sheet and return dict of relevant strings
 function convert(vals){
   let dict = {};
@@ -141,6 +143,7 @@ function convert(vals){
   print(dict);
   return dict;
 }
+
 // check for keyboard events
 function keyPressed() {
   switch(key) {
