1363c8ee6d4ab36c802dbc10a7f45d3fdf7fa16b
Andrew Chalfant <andrewlchalfant@gmail.com>
2020-05-29 21:13:32 -0400

    graphing library example

--- project2/sketch.js
+++ project2/sketch.js
@@ -8,13 +8,13 @@ let dataMap;
 let url = "https://sheets.googleapis.com/v4/spreadsheets/1tjvDbvUsSogN2CK5RrXXntTHlWSRl-1T4UiUJJLw5gA/values/";
 let sheet_name = "live_tweets" + "!";
 var r_max = 5000; //how many datapoints to fetch
-let range = "B2:B" + r_max;
+let range = "C2:C" + r_max;
 let key = "?key=AIzaSyCEQ1fTLIunpWw7aMdFXgfyQ6lvkN4kiZc";
 let sheets = url + sheet_name + range + key;
 
-let tex_white;
-let tex_red;
+
 let song;
+let plot;
 
 let params = {
     scale: 15,
@@ -28,8 +28,9 @@ function preload() {
 
 function setup() { 
   pixelDensity(1);
-
   var canvas = createCanvas(windowWidth, windowHeight);
+  plot = new GPlot(this);
+
   setAttributes('antialias', true);
   
   // define initial state
@@ -52,7 +53,9 @@ function windowResized() {
 
 
 function draw(){
+  background(0);
   if (!resp) {
+    text("Loading", windowWidth/10, windowHeight/2 - 100);
     return; //wait for http response
   }
   if (flip) {
@@ -69,11 +72,25 @@ function draw(){
     timer = millis();
   }
 
-  textSize(params.scale);
-  background(0);
-  text(dataMap[0], windowWidth/10, windowHeight/2 - 100);
-  text(dataMap[2000], windowWidth/10, windowHeight/2);
-  text(dataMap[3500], windowWidth/10, windowHeight/2 + 100);
+    plot.setPos(0, 0);
+    plot.setOuterDim(500, 500);
+
+    // Add the points
+    plot.setPoints(dataMap);
+
+    // Set the plot title and the axis labels
+    plot.setTitleText("A very simple example");
+    plot.getXAxis().setAxisLabelText("x axis");
+    plot.getYAxis().setAxisLabelText("y axis"); 
+    //plot.setLogScale("y");
+    plot.activatePanning();
+    // Draw it!
+    draw_plot(this);
+    plot.setPointColor(this.color(50, 50, 255, 20));
+  //textSize(params.scale);
+  //text(dataMap[0], windowWidth/10, windowHeight/2 - 100);
+  //text(dataMap[2000], windowWidth/10, windowHeight/2);
+  //text(dataMap[3500], windowWidth/10, windowHeight/2 + 100);
   fill(255, 255, 255);
   noStroke();
 }
@@ -83,24 +100,12 @@ function convert(vals){
   let arr = [];
   for (let i = 0; i < vals.length; i++) {
     var str = vals[i][0];
-    var tweet = str.split("]")[1]; //store username and if rt
-    var temp = tweet.split(" ");
-    temp.forEach(function(x) {
-      if (dict.has(x)) {
-        dict.set(x, dict.get(x) + 1);
-      } else {
-        dict.set(x, 1);
-      }
-    });
+    //var tweet = str.split("]")[1]; //store username and if rt
+    //var temp = tweet.split(" ");
     //tweet = tweet.split(": ")[1];
-    arr.push(tweet);
+    arr[i]= new GPoint(i, str);
   }
 
-  for (let [key, value] of dict) {
-    if (value > 1000 && key.length > 2) {
-      print(key + " and " + value);
-    }
-  }
   print(arr);
   return arr;
 }
@@ -115,3 +120,21 @@ function keyPressed() {
       break;
   }
 }
+
+
+function draw_plot(p) {
+    // Clean the canvas
+    p.background(150);
+
+    // Draw the plot
+    plot.beginDraw();
+    plot.drawBackground();
+    plot.drawBox();
+    plot.drawXAxis();
+    plot.drawYAxis();
+    plot.drawTopAxis();
+    plot.drawRightAxis();
+    plot.drawTitle();
+    plot.drawPoints();
+    plot.endDraw();
+  };
