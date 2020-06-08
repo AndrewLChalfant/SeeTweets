11bd75c5fc7c91fb45ce486fd0b6a953a2d02d5b
Andrew Chalfant <andrewlchalfant@gmail.com>
2020-06-08 12:28:38 -0400

    font testing and google tracking

--- project3/sketch.js
+++ project3/sketch.js
@@ -4,36 +4,40 @@ let resp2;
 
 var x = 0;
 let flip = true; 
+let fade = 850;
 let timer = 0;
 let timer2 = 0;
 let dataMap;
-let button;
 
 let url = "https://sheets.googleapis.com/v4/spreadsheets/176z44O-mgCBX20skzYfsT7Kx1yibE4jguVcvXEHpn3M/values/";
 let sheet_name = "live" + "!";
 var r_max = 100; //how many datapoints to fetch
 let range = "C2:C" + r_max;
+let range2 = "B2:B" + r_max;
 let key = "?key=AIzaSyC0A1JV1C3_JGvLh-3hKC3klIcNXCxUdYY";
 
 let sheets_nums = url + sheet_name + range + key;
-let sheets_words = url + sheet_name + "B2:B" + r_max + key;
+let sheets_words = url + sheet_name + range2 + key;
 let index = 0;
+
+let fontReglar;
+
+//default backgrond color
 let h = 25;
 let s = 175;
 let b = 225;
+function preload() {
+  fontRegular =loadFont('Song.ttf');
+}
 
 function setup() {
   colorMode(HSB, 255);
   pixelDensity(1);
+
   var canvas = createCanvas(windowWidth, windowHeight);
   setAttributes('antialias', true);
-  
-  // define initial state
-  var state = {
-    distance : 200,
-  };
-
   update_call();
+  textFont(fontRegular);
 } 
 
 
@@ -45,6 +49,7 @@ function windowResized() {
 function draw(){
   let c = color(h, s, b);
   background(c);
+
   if (!resp || !resp2) {
     textSize(100);
     //text("Loading", windowWidth/10, windowHeight/2 - 100);
@@ -58,29 +63,43 @@ function draw(){
     flip = false;
   }
 
-  if (millis() >= 8000 + timer2) { //update once per 30 secs
+  if (millis() >= 7500 + timer2 + Math.random()) { //update once per 8 secs
     index += 1;
+    fade = 850;
     timer2 = millis();
-    if (index == dataMap.length -1) {
+    if (index >= dataMap.length - 2) {
       index = 0;
     }
   }
-    if (millis() >= 60000 + timer) {
-      resp = null;
-      update_call();
-      timer = millis();
-      return;
+
+  if (fade > 0) {
+    fade -= 2.1;
+  }
+
+
+
+  if (millis() >= 60000 + timer) {
+    resp = null;
+    update_call();
+    timer = millis();
+    return;
   }
   
   textSize(10);
   fill(255);
-  text("Displaying happy live tweets - anonymously from around the world", windowWidth/50, 20, 400, 200);
+
+  textAlign(LEFT);
+  textFont('Helvetica');
+  text("Displaying happy live tweets - anonymously from around the world", windowWidth/50, 20, 400, 300);
   text("Andrew Chalfant 2020", windowWidth/50, windowHeight - 25, 400, 200);
-  
-  textSize(25);
-  if (dataMap) {
-    fill(255);
-    text(dataMap[index][0], windowWidth/2 - 200, windowHeight/2 - 100, 400, 200);
+  //textFont(fontRegular);
+
+  textSize(35);
+  if (dataMap[index]) {
+    fill(0, 0, 255, fade);
+
+    textAlign(CENTER);
+    text(dataMap[index][0], windowWidth/2 - 300, windowHeight/2 - 125, 600, 300);
     stroke(255);
     fill(0, 0, 0, 0);
     //ellipse(windowWidth/2, windowHeight/2 - 85, dataMap[index][1] * 50, dataMap[index][1] * 50);
@@ -97,9 +116,9 @@ function convert(vals){
     var str = resp2.values[i][0];
     var score = vals[i][0];
     var tweet = str.split("]")[1]; //store username and if rt
-    tweet = tweet.replace("&", "and");
-    //var temp = tweet.split(" ");
-    //tweet = tweet.split(": ")[1];
+    tweet = tweet.replace(/&amp;/g, "");
+    tweet = "\"" + tweet.replace(/&lt;/g, "<") + "\"";
+
     if (parseInt(score) > 2 && !tweet.includes("birthday") && !tweet.includes("bday")) {
       arr.push([tweet, parseInt(score)]);
     }
