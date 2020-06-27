08ba8540d8724d9828ed267acdabd37f324b9eae
Andrew Chalfant <andrewlchalfant@gmail.com>
2020-06-27 19:00:16 -0400

    added about and contact pages

---
+++ scripts/draw.js
@@ -0,0 +1,184 @@
+let text_input;
+let name_input;
+let button;
+
+let heightbox = 0;
+let heightSpeed = 8;
+let errorFill = 0;
+let drawError = false;
+let contactMessage = "";
+
+//draw supporting graphics for main page
+function draw_back_text() {
+  if (heightbox > 0) {
+    heightbox -= heightSpeed;
+  }
+
+  drawHeader();
+  fill(0, 0, 255, 0);
+  stroke(0, 0, 255, 100);
+  rect(windowWidth/2 - 310, windowHeight/2 - 200 - heightbox/2, 620, 300 + heightbox, 20);
+
+  noStroke();
+  fill(255);
+  rect(windowWidth/2 + 150, windowHeight/2 + 110, 150, 20, 5);
+  //textFont(fontRegular);
+
+  textSize(10);
+  fill(c);
+  text("Share on Twitter", windowWidth/2 + 185, windowHeight/2 + 116, 600, 300);
+}
+
+//draw main tweet text
+function draw_text() {
+  draw_back_text();
+
+  if (dataMap[index]) {
+    tweet = dataMap[index][0];
+    lastTweet = tweet;
+
+    if (tweet.length > 180) {
+      textSize(22); 
+    } else if (tweet.length > 130) {
+      textSize(25);
+      //fade_rate = 2;
+    } else if (tweet.length < 30) {
+      textSize(40);
+    } else {
+      textSize(33);
+      //fade_rate = 2;
+    }
+    
+    textAlign(CENTER);
+    noStroke();
+    fill(0, 0, 255, fade);
+    text(tweet, windowWidth/2 - 300, windowHeight/2 - 125, 600, 300);
+   
+    textSize(15);
+    textAlign(LEFT);
+
+    var location = "unknown";
+    if (dataMap[index][2] && dataMap[index][2] != "" && dataMap[index][2].length < 20) {
+      location = dataMap[index][2];
+      text("from: " + location, windowWidth/2 - 310, windowHeight/2 + 115, 600, 300);
+    }
+
+    stroke(255);
+    fill(0, 0, 0, 0);
+    //ellipse(windowWidth/2, windowHeight/2 - 85, dataMap[index][1] * 50, dataMap[index][1] * 50);
+    strokeWeight(4);
+    noStroke();
+    //b += dataMap[index][1] / 500;
+  }
+}
+
+//draw the about page
+function draw_about() {
+  if (button) {
+    text_input.hide();
+    name_input.hide();
+    button.hide();
+  }
+
+  if (heightbox < 200) {
+    heightbox += heightSpeed;
+  }
+
+  c = color(h, s, b);
+  background(c);
+  drawHeader();
+
+  fill(0, 0, 255, 0);
+  stroke(0, 0, 255, 100);
+  rect(windowWidth/2 - 310, windowHeight/2 - 200 - heightbox/2, 620, 300 + heightbox, 20);
+  //rect(windowWidth/2 - 310, windowHeight/2 - 300, 620, 600, 20);
+
+  fill(0, 0, 255, 0 + (1.2 * heightbox));
+  noStroke();
+  textSize(50);
+  text("About", windowWidth/2 - 70, windowHeight/2 - 250, 600, 300);
+  text("x", windowWidth/2 + 250, windowHeight/2 - 290, 600, 300);
+
+  textSize(20);
+  let temp_text = "Everyday we are bombarded with negative content distributed over the internet. Unfortunately this abundance of negativity often drowns out more uplifting messages.\n\nThe purpose of SeeTweets is simple: to highlight positive content. SeeTweets anonymously displays live tweets from around the world in real time.\n\nSeeTweet is an open source project built by Andrew Chalfant with p5.js and Google Scripts. To learn more click here.";
+  text(temp_text, windowWidth/2 - 230, windowHeight/2 - 180, 500, 500);
+}
+
+
+//draw the contact page
+function draw_contact() {
+  //text after button press
+  if (drawError) {
+    print("Draw error");
+    fill(0, 0, 255, errorFill);
+    text(contactMessage, text_input.x, button.y + button.height + 10, 500, 500);
+  }
+
+  if (errorFill >= 0) {
+    errorFill -= 4;
+  } else {
+    drawError = false;
+    errorFill = 1000;
+  }
+
+  if (heightbox < 200) {
+    heightbox += heightSpeed;
+  }
+
+  if (button) {
+    name_input.show();
+    text_input.show();
+    button.show();
+  }
+
+  fill(255);
+  c = color(h, s, b);
+  //background(c);
+  drawHeader();
+
+  fill(0, 0, 255, 0);
+  stroke(0, 0, 255, 100);
+  rect(windowWidth/2 - 310, windowHeight/2 - 200 - heightbox/2, 620, 300 + heightbox, 20);
+
+  fill(0, 0, 255, 0 + (1.2 * heightbox));
+  noStroke();
+  textSize(50);
+  text("Contact", windowWidth/2 - 90, windowHeight/2 - 250, 600, 300);
+  text("x", windowWidth/2 + 250, windowHeight/2 - 290, 600, 300);
+  
+  textSize(20);
+  name_input.position(windowWidth/2 - 150, windowHeight/2 - 150);
+  name_input.size(310, 30);
+  name_input.style("border", "none");
+  text("Name:", name_input.x, name_input.y - 20, 600, 300);
+
+  text_input.position(windowWidth/2 - 150, windowHeight/2 - 80);
+  text_input.size(name_input.width, name_input.height * 4);
+  text_input.style("border", "none");
+  text("Message:", text_input.x, text_input.y - 20, 600, 300);
+
+  //style send button
+  button.position(text_input.x, text_input.y + text_input.height + 20);
+  button.size(text_input.width, text_input.height * 0.5);
+
+  button.style('background-color', c);
+  button.style('color', "white");
+  button.style('font-size', 20);
+  button.style('border', '2px solid white');
+  button.style('opacity', 0.9);
+  button.style("border-radius", "6px");
+
+  button.mousePressed(send_contact); 
+}
+
+//draw default header
+function drawHeader() {
+  textSize(20);
+  fill(255);
+  textFont('Helvetica');
+  text("Displaying live tweets - anonymously from around the world", windowWidth/50, 20, 1000, 300);
+
+  textSize(20);
+  text("About", windowWidth - 200, 20, 400, 200);
+  text("Contact", windowWidth - 110, 20, 400, 200);
+}
---
+++ scripts/sketch.js
@@ -0,0 +1,228 @@
+//SOURCE CODE HERE: https://github.com/AndrewLChalfant/SeeTweets
+
+p5.disableFriendlyErrors = true;
+let resp;
+let resp2;
+let page = 0;
+
+let flip = true; 
+let fade = 0;
+let fade_rate = 10;
+let fade_cap = 2300;
+let timer = 0;
+let timer2 = 0;
+
+let dataMap;
+let flip_switch = true;
+let about_switch = false;
+
+var click;
+var email;
+
+let url = "https://sheets.googleapis.com/v4/spreadsheets/176z44O-mgCBX20skzYfsT7Kx1yibE4jguVcvXEHpn3M/values/";
+let sheet_name = "live" + "!";
+var r_max = 100; //how many datapoints to fetch
+let range = "C2:C" + r_max;
+let range2 = "B2:B" + r_max;
+let key = "?key=AIzaSyC0A1JV1C3_JGvLh-3hKC3klIcNXCxUdYY";
+
+let sheets_nums = url + sheet_name + range + key;
+let sheets_words = url + sheet_name + range2 + key;
+let index = 0;
+
+let div;
+let lastTweet = "";
+
+//default backgrond color
+let h = 25;
+let s = 175;
+let b = 225;
+let c;
+
+function preload() {
+  //fontRegular =loadFont('Song.ttf');
+}
+
+function setup() {
+  colorMode(HSB, 255);
+  pixelDensity(1);
+
+  var canvas = createCanvas(windowWidth, windowHeight);
+  setAttributes('antialias', true);
+  update_call();
+  //textFont(fontRegular);
+  index = int(Math.random() * 30);
+  name_input = createInput('');
+  text_input = createElement("textarea", "");
+  button = createButton('Send');
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+function draw(){
+  c = color(h, s, b);
+  background(c);
+
+  //loading screen
+  if (millis() < 2000) {
+    background(c);
+    fill(0, 0, 255, 750 - millis()/3);
+    textSize(80);
+    text("SeeTweets", windowWidth/2 - 200, windowHeight/2 - 80, 600, 300);
+    noStroke();
+    rect(windowWidth/2 - 205, windowHeight/2, 420, 8);
+    return;
+  }
+
+  if (page == 0 && (!resp || !resp2)) {
+    textSize(100);
+    text(lastTweet, windowWidth/2 - 300, windowHeight/2 - 125, 600, 300);
+    flip = true;
+    return; //wait for http response
+  }
+
+  if (flip) {
+    dataMap = convert(resp.values); 
+    print("Converting");
+    flip = false;
+  }
+
+  if (millis() >= 8000 + timer2 + Math.random() || click) { //update once per 8 secs 
+    index += 1;
+    fade = 0;
+    flip_switch = true;
+    timer2 = millis();
+    if (index >= dataMap.length - 2) {
+      index = 0;
+    }
+    click = false;
+  }
+
+  if (flip_switch && fade < fade_cap) {
+    fade += 10;
+  }
+  if (fade >= fade_cap) {
+    flip_switch = false;
+  }
+
+  if (!flip_switch && fade > -10) {
+    fade -= fade_rate;
+  }
+
+  if (millis() >= 30000 + timer) {
+    resp = null;
+    update_call();
+    timer = millis();
+    return;
+  }
+
+  if (page == 1) {
+    draw_about();
+  } else if (page == 2) {
+    draw_contact();
+  } else if (page == 0) {
+    if (button) {
+      text_input.hide()
+      button.hide();
+      name_input.hide();
+    }
+    draw_text();
+  }
+}
+
+//get tweets from google sheet and return dict of relevant strings
+function convert(vals){
+  let arr = [];
+  for (let i = 0; i < vals.length; i++) {
+    var str = resp2.values[i][0];
+    var score = vals[i][0];
+
+    var location = str.split("&")[1];
+    var tweet = str.split("&")[2]; //store username and if rt
+    tweet = "\"" + tweet + "\"";
+
+    arr.push([tweet, parseInt(score), location]);
+  }
+
+  print(arr);
+  return arr;
+}
+
+function update_call() {
+  httpGet(sheets_nums, 'jsonp', false, function(response) {
+    resp = response;
+  });
+  httpGet(sheets_words, 'jsonp', false, function(response2) {
+    resp2 = response2;
+  });
+}
+
+function mousePressed() {
+  if (clickHelper(windowWidth - 200, windowWidth - 130, 20, 50)) {
+    page = 1; //visit about page
+  }
+
+  if (clickHelper(windowWidth - 110, windowWidth - 40, 20, 50)) {
+    page = 2; //visit contact page
+  }
+
+  if (page == 0) { //home page
+    if (clickHelper(windowWidth/2 + 165, windowWidth/2 + 465, windowHeight/2 + 114, windowHeight/2 + 130)) { //share tweet
+        let message = "I liked a tweet from SeeTweets.com: " + lastTweet; // %23SeeTweet";
+        window.open('https://twitter.com/intent/tweet/?text=' + message + '&amp;url=" target="_blank"');
+    }
+
+    if (clickHelper(windowWidth/2 - 300, windowWidth/2 + 300, windowHeight/2 - 125, windowHeight/2 + 100)) {
+        click = true; //skip tweet
+    }
+
+  } else if (page == 1) { //about page
+    if (clickHelper(windowWidth/2 + 250, windowWidth/2 + 300, windowHeight/2 - 290, windowHeight/2 - 220)) {
+      page = 0; 
+    }
+
+    if (clickHelper(windowWidth/2 - 100, windowWidth/2 + 100, windowHeight/2 + 100, windowHeight/2 + 200)) {
+      window.open("https://github.com/AndrewLChalfant/SeeTweets");
+    }
+
+  } else if (page == 2) { //contact page
+    if (clickHelper(windowWidth/2 + 250, windowWidth/2 + 300, windowHeight/2 - 290, windowHeight/2 - 220)) {
+      page = 0; 
+    }
+
+  }
+}
+
+function clickHelper(x1, x2, y1, y2) {
+  if (mouseX > x1  && mouseY > y1) { //share tweet
+    if (mouseX < x2 && mouseY < y2) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function send_contact() {
+  if (text_input.value().trim() === "" || name_input.value().trim() === "") {
+    drawError = true;
+    contactMessage = "Error: Invalid input";
+  } else { 
+    Email.send({
+    SecureToken : "8f3f3aff-06d2-493b-aa98-1d135b73a48b",
+    To : "achalfan@terpmail.umd.edu", 
+    From : "noreply@seetweets.com",
+    Subject : "SeeTweets User Input from " + name_input.value(),
+    Body : text_input.value()
+    }).then(
+          message => feedback_sent()
+      );
+    }
+}
+
+function feedback_sent(){
+  drawError = true;
+  contactMessage = "Message sent successfully";
+}
